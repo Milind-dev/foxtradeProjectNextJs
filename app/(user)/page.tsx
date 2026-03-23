87c7ff2eba838800f19f83c9a4bb3baf5dd282fa
Milind-dev <milinddev1011@gmail.com>
2026-03-23 15:49:47 +0530

    image

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
+import candlestick from "../candlestick-chart_6354012.png";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="text-grey-600 flex justify-center text-[222%]">
-        User Bitcoin Datas{" "}
-      </h1>
+      <h1 className="text-center py-3 text-3xl">User Bitcoin Datas </h1>
+      <div className="border  py-5">
+        <h1 className="text-grey-600 flex justify-center text-[222%]">
+          <Image src={candlestick} width="200" height="500" alt="candlestick" />
+        </h1>
+      </div>
     </div>
   );
 }
+// candlestick - chart_6354012;
